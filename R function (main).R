f31adb3603c6bb477ce422c4af05bd00b9bd1013
ewongtoi <ewongtoi@gmail.com>
2020-11-20 20:34:23 -0800

    updated variable names, functions to tidyverse style

--- R function (main).R
+++ R function (main).R
@@ -1,5 +1,5 @@
 # Calculate sample size with given power
-MRT_bin_samplesize.F <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
+calculate_mrt_bin_samplesize_f <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
                                  f_t,             ## f(t)    # TQ: will assume this f_t object is matrix of dimension T * p
                                  g_t,             ## g(t)    # TQ: will assume this g_t object is matrix of dimension T * q
                                  beta,            ## beta_0
@@ -12,28 +12,28 @@ MRT_bin_samplesize.F <- function(avail_pattern,  ## E[I_t]  # TQ: will assume th
     p <- length(beta)
     q <- length(alpha)
     
-    M_and_Sigma <- computeMSigma(avail_pattern, f_t, g_t, beta, alpha, p_t)
-    M.Matrix <- M_and_Sigma$M
-    Sigma.Matrix <- M_and_Sigma$Sigma
+    m_and_sigma <- compute_m_sigma(avail_pattern, f_t, g_t, beta, alpha, p_t)
+    m_matrix <- m_and_sigma$m
+    sigma_matrix <- m_and_sigma$sigma
     
     ## Setting up the function that we will ultimately solve to get the sample size
-    powerF <- function(n){
+    power_f <- function(n){
         ## Lambda as a function of x (i.e., the sample size)
-        lambda <- function(x){
-            as.numeric(x * t(beta) %*% solve(solve(M.Matrix) %*% Sigma.Matrix %*% t(solve(M.Matrix))) %*% beta)
+        compute_lambda <- function(x){
+            as.numeric(x * t(beta) %*% solve(solve(m_matrix) %*% sigma_matrix %*% t(solve(m_matrix))) %*% beta)
         }
         
-        righthand.side <- pf(q=qf(p=(1-gamma), df1=p, df2=n-q-p), df1=p, df2=n-q-p, ncp=lambda(n))
-        lefthand.side = b
-        return(righthand.side - lefthand.side)
+        right_hand_side <- pf(q=qf(p=(1-gamma), df1=p, df2=n-q-p), df1=p, df2=n-q-p, ncp=compute_lambda(n))
+        left_hand_side = b
+        return(right_hand_side - left_hand_side)
     }
     
-    sample_size <- uniroot(powerF, lower=p+q+1, upper=1000000)$root
+    sample_size <- uniroot(power_f, lower=p+q+1, upper=1000000)$root
     return(sample_size)
 }
 
 
-computeMSigma <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
+compute_m_sigma <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
                           f_t,             ## f(t)    # TQ: will assume this f_t object is matrix of dimension T * p
                           g_t,             ## g(t)    # TQ: will assume this g_t object is matrix of dimension T * q
                           beta,            ## beta_0
@@ -44,8 +44,8 @@ computeMSigma <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is v
     q <- length(alpha)
     
     ## The M and Sigma matrices (needed to compute lambda)
-    M.Matrix <- matrix(data=0, nrow=length(beta), ncol=length(beta))
-    Sigma.Matrix <- matrix(data=0, nrow=length(beta), ncol=length(beta))
+    m_matrix <- matrix(data=0, nrow=length(beta), ncol=length(beta))
+    sigma_matrix <- matrix(data=0, nrow=length(beta), ncol=length(beta))
     for (i in 1:length(p_t)){## For each decision point (T = total # of decision points is taken as the length of p_t, for now)
         
         # breaking down steps to identify bug and improve robustness of code
@@ -60,19 +60,19 @@ computeMSigma <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is v
         
         this_f_t_f_t <- outer(this_f_t, this_f_t) # this is f_t %*% f_t^T
         
-        ThisM <- as.numeric(avail_pattern[i] * (exp(p_t[i] * this_f_t_times_beta)) * exp(this_g_t_times_alpha) * (1-p_t[i]) * p_t[i]) * this_f_t_f_t
-        stopifnot("matrix" %in% class(ThisM) & all(dim(ThisM) == c(p, p))) # added a check of dimension
+        this_m <- as.numeric(avail_pattern[i] * (exp(p_t[i] * this_f_t_times_beta)) * exp(this_g_t_times_alpha) * (1-p_t[i]) * p_t[i]) * this_f_t_f_t
+        stopifnot("matrix" %in% class(this_m) & all(dim(this_m) == c(p, p))) # added a check of dimension
         
-        M.Matrix <- M.Matrix + ThisM  ## A running sum so that we end up with each entry being the sum of that entry across all time points
+        m_matrix <- m_matrix + this_m  ## A running sum so that we end up with each entry being the sum of that entry across all time points
         
-        ThisSigma <- as.numeric(avail_pattern[i] * (exp(2 * p_t[i] * this_f_t_times_beta)) * exp(this_g_t_times_alpha) * (1-p_t[i]) * p_t[i] * ((1-p_t[i]) * exp(-1 * this_f_t_times_beta) + p_t[i] - exp(this_g_t_times_alpha))) * this_f_t_f_t
-        Sigma.Matrix <- Sigma.Matrix + ThisSigma
-        stopifnot("matrix" %in% class(ThisSigma) & all(dim(ThisSigma) == c(p, p)))
+        this_sigma <- as.numeric(avail_pattern[i] * (exp(2 * p_t[i] * this_f_t_times_beta)) * exp(this_g_t_times_alpha) * (1-p_t[i]) * p_t[i] * ((1-p_t[i]) * exp(-1 * this_f_t_times_beta) + p_t[i] - exp(this_g_t_times_alpha))) * this_f_t_f_t
+        sigma_matrix <- sigma_matrix + this_sigma
+        stopifnot("matrix" %in% class(this_sigma) & all(dim(this_sigma) == c(p, p)))
     }
     
-    return(list(M = M.Matrix, Sigma = Sigma.Matrix))
+    return(list(m = m_matrix, sigma = sigma_matrix))
     
 }
 
-MRT_bin_samplesize.F()
+calculate_bin_samplesize_f()
 
