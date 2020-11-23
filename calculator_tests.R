c7751feca836f77113ce33b92e4b9b06d5ed1cc7
ewongtoi <ewongtoi@gmail.com>
2020-11-22 20:23:53 -0800

    started test file using TQ's sample

---
+++ calculator_tests.R
@@ -0,0 +1,74 @@
+library(testthat)
+
+
+
+
+
+
+# TQ added
+set.seed(1)
+
+
+
+total_dp <- 10 # total number of decision points
+# p and q are not directly used in the function, so I commented them out.
+# p <- 2
+# q <- 2
+
+tau_t <- rep(0.8, total_dp) # expected availability E(I_t) for t = 1,...,total_dp
+
+p_t <- rep(0.4, total_dp) # randomization probability over time
+gamma <- 0.05 # type I error
+b <- 0.2 # type II error; power = 1 - b
+
+### specify g_t and alpha ###
+g_t <- cbind(rep(1, total_dp), 1:total_dp)  # f_t = (1,g_t)
+alpha <- as.matrix(c(-0.2, -0.1), ncol = 1)
+
+# checking that probability E(Y_{t+1} = 1 | I_t = 1, A_t = 0) should always be between 0 and 1.
+mu0_t <- exp(g_t %*% alpha) # E(Y_{t+1} = 1 | I_t = 1, A_t = 0) for t = 1,...,total_dp
+mu0_t # look at the mu0_t values
+stopifnot(all(mu0_t < 1) & all(mu0_t > 0))
+
+### specify f_t and beta ###
+f_t <- cbind(rep(1, total_dp), 1:total_dp)  # f_t = (1, t)
+beta <- as.matrix(c(0.15, - 0.01), ncol = 1)
+
+# checking that probability E(Y_{t+1} = 1 | I_t = 1, A_t = 1) should always be between 0 and 1.
+mee_t <- f_t %*% beta # MEE(t) for t = 1,...,total_dp
+mu1_t <- mu0_t * exp(mee_t) # E(Y_{t+1} = 1 | I_t = 1, A_t = 1) for t = 1,...,total_dp
+mu1_t # look at the mu1_t values
+stopifnot(all(mu1_t < 1) & all(mu1_t > 0))
+
+
+p_t <- rep(0.4, total_dp) # randomization probability over time
+gamma <- 0.05 # type I error
+b <- 0.2 # type II error; power = 1 - b
+
+### specify g_t and alpha ###
+g_t <- cbind(rep(1, total_dp), 1:total_dp)  # f_t = (1,g_t)
+alpha <- as.matrix(c(-0.2, -0.1), ncol = 1)
+
+# checking that probability E(Y_{t+1} = 1 | I_t = 1, A_t = 0) should always be between 0 and 1.
+mu0_t <- exp(g_t %*% alpha) # E(Y_{t+1} = 1 | I_t = 1, A_t = 0) for t = 1,...,total_dp
+mu0_t # look at the mu0_t values
+stopifnot(all(mu0_t < 1) & all(mu0_t > 0))
+
+### specify f_t and beta ###
+f_t <- cbind(rep(1, total_dp), 1:total_dp)  # f_t = (1, t)
+beta <- as.matrix(c(0.15, - 0.01), ncol = 1)
+
+# checking that probability E(Y_{t+1} = 1 | I_t = 1, A_t = 1) should always be between 0 and 1.
+mee_t <- f_t %*% beta # MEE(t) for t = 1,...,total_dp
+mu1_t <- mu0_t * exp(mee_t) # E(Y_{t+1} = 1 | I_t = 1, A_t = 1) for t = 1,...,total_dp
+mu1_t # look at the mu1_t values
+stopifnot(all(mu1_t < 1) & all(mu1_t > 0))
+
+context("testing that outputs match original function")
+test_that(
+  "check TQ's sample",
+  {
+    expect_equal(MRT_bin_samplesize.F(tau_t, f_t, g_t, beta, alpha, p_t, gamma, b),
+  calculate_mrt_bin_samplesize_f(tau_t, f_t, g_t, beta, alpha, p_t, gamma, b))
+  }
+)
