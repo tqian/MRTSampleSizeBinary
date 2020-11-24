0dd74361e787baccf0fa3069948a3ad6e61186ed
ewongtoi <ewongtoi@gmail.com>
2020-11-23 21:24:49 -0800

    implemented function.

--- calculate_mrt_bin_power_f.R
+++ calculate_mrt_bin_power_f.R
@@ -1,3 +1,6 @@
+
+# returns power of test given a fixed sample size and specified 
+# null/alternative hypotheses
 calculate_mrt_bin_power_f <- function(avail_pattern,  
                                            f_t,             
                                            g_t,             
@@ -5,8 +8,17 @@ calculate_mrt_bin_power_f <- function(avail_pattern,
                                            alpha,           
                                            p_t,             
                                            gamma,          
-                                           n,
-                                           exact=FALSE)               
+                                           n)               
 {
+  p <- length(beta)
+  q <- length(alpha)
   
+  m_and_sigma <- compute_m_sigma(avail_pattern, f_t, g_t, beta, alpha, p_t)
+  m_matrix <- m_and_sigma$m
+  sigma_matrix <- m_and_sigma$sigma
+  
+  return(pf(q=qf(p=(1-gamma), df1=p, df2=n-q-p), 
+     df1=p, 
+     df2=n-q-p, 
+     ncp=compute_ncp(n, beta, m_matrix, sigma_matrix)))
 }
