ceceebdfef8a639a6f3c220f345669afe244bc14
ewongtoi <ewongtoi@gmail.com>
2020-11-23 20:37:04 -0800

    started function to find power at a fixed sample size

---
+++ calculate_power.R
@@ -0,0 +1,12 @@
+calculate_mrt_bin_power_f <- function(avail_pattern,  
+                                           f_t,             
+                                           g_t,             
+                                           beta,            
+                                           alpha,           
+                                           p_t,             
+                                           gamma,          
+                                           n,
+                                           exact=FALSE)               
+{
+  
+}
