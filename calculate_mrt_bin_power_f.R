e5ced672e357959fc233f10cfc3f5b579d5a5fb4
ewongtoi <ewongtoi@gmail.com>
2020-11-24 17:07:06 -0800

    added roxygen2 skeleton

--- calculate_mrt_bin_power_f.R
+++ calculate_mrt_bin_power_f.R
@@ -1,6 +1,18 @@
-
-# returns power of test given a fixed sample size and specified 
-# null/alternative hypotheses
+#' Returns power of a test given a prespecified sample size in the context of a binary MRT.
+#'
+#' @param avail_pattern A vector of length T that is the average availability at each time point
+#' @param f_t           Defines marginal excursion effect MEE(t) under alternative together with beta
+#' @param g_t           Defines success probability null curve together with alpha
+#' @param beta          Defines marginal excursion effect MEE(t) under alternative together with g_t
+#' @param alpha         Defines success probability null curve together with f_t
+#' @param p_t           Randomization probability at each time point
+#' @param gamma         Desired Type I error
+#' @param n             Number of participants
+#'
+#' @return              Power of the test.
+#' @export
+#'
+#' @examples
 calculate_mrt_bin_power_f <- function(avail_pattern,  
                                            f_t,             
                                            g_t,             
