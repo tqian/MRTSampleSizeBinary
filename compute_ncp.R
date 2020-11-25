28173ee574be86f18972960f0742afc3e0ef6b62
ewongtoi <ewongtoi@gmail.com>
2020-11-24 17:01:48 -0800

    added roxygen2 skeleton, not finished filling out yet

--- compute_ncp.R
+++ compute_ncp.R
@@ -1,10 +1,21 @@
-# helper function for calculate_mrt_bin_samplesize_f
-# computes the non-centrality parameter for the F distribution when solving for
-# sample size
+#' Computes the non-centrality parameter for a an F distributed random variable
+#' in the context of a binary MRT. This is primarily a helper function for
+#' calculate_mrt_bin_power_f and calculate_mrt_bin_samplesize_f.
+#'
+#' @param x            Sample size
+#' @param beta         Marginal excursion effect, assumed dimension p
+#' @param m_matrix     "Bread" of sandwich estimator for variance
+#' @param sigma_matrix "Meat" of sandwich estimator for variance
+#'
+#' @return Returns non-centrality parameter for an F distributed random
+#'   variable.
+#' @export
+#'
+#' @examples
 compute_ncp <- function(x, beta, m_matrix, sigma_matrix){
-  as.numeric(x * t(beta) %*%
+  return(as.numeric(x * t(beta) %*%
                solve(solve(m_matrix) %*%
                        sigma_matrix %*%
                        t(solve(m_matrix))) %*% 
-               beta)
+               beta))
 }
