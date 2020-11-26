a8d3c3ca32d03ce62a3f7f3f5ebb9249821340b7
ewongtoi <ewongtoi@gmail.com>
2020-11-25 20:47:05 -0800

    check dimensions and ranks of matrices before multiplying/inverting

--- compute_ncp.R
+++ compute_ncp.R
@@ -13,6 +13,25 @@
 #'
 #' @examples
 compute_ncp <- function(x, beta, m_matrix, sigma_matrix){
+  if(det(m_matrix) == 0){
+    stop("m_matrix must be nonsingular")
+  }
+  
+  if(length(beta) != dim(m_matrix[1])){
+    stop("Dimensions of beta and m_matrix do not agree")
+  }
+  
+  if(dim(m_matrix)[1] != dim(sigma_matrix)[2] | 
+     dim(m_matrix[2] != dim(sigma_matrix[1]))){
+    stop("Dimensions of m_matrix and sigma_matrix do not agree")
+  }
+  
+  if(det(solve(m_matrix) %*%
+         sigma_matrix %*%
+         t(solve(m_matrix))) == 0){
+    stop("sigma_matrix must be nonsingular")
+  }
+  
   return(as.numeric(x * t(beta) %*%
                solve(solve(m_matrix) %*%
                        sigma_matrix %*%
