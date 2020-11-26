c607e4686916fd88db894b393a02f6d6142b85e4
ewongtoi <ewongtoi@gmail.com>
2020-11-25 17:20:10 -0800

    added tests for newly added warning messages and different dimensions for f/g

--- calculator_tests.R
+++ calculator_tests.R
@@ -1,5 +1,7 @@
 library(testthat)
 
+
+# simulate data for tests -------------------------------------------------
 # TQ added
 set.seed(1)
 
@@ -66,13 +68,126 @@ mu1_t <- mu0_t * exp(mee_t)
 mu1_t # look at the mu1_t values
 stopifnot(all(mu1_t < 1) & all(mu1_t > 0))
 
+
+
+g_new <- cbind(rep(1, total_dp), 1:total_dp, (1:total_dp)^2)
+alpha_new <- as.matrix(c(-0.2, -0.1, .01), ncol = 1)
+
+f_new <- cbind(rep(1, total_dp), 1:total_dp, (1:total_dp)^2) # f_t = (1, t)
+beta_new <- as.matrix(c(0.15, - 0.01, -.1), ncol = 1)
+
+# calculate_mrt_bin_samplesize_f tests ------------------------------------
 context("testing that outputs match original function")
 test_that(
   "check TQ's sample",
   {
     expect_equal(
-      MRT_bin_samplesize.F(tau_t, f_t, g_t, beta, alpha, p_t, gamma, b),
       calculate_mrt_bin_samplesize_f(tau_t, f_t, g_t, beta, 
-                                     alpha, p_t, gamma, b))
+                                     alpha, p_t, gamma, b, TRUE),
+    274.0055127)
   }
 )
+
+test_that(
+  "check that the round up feature is working",
+  {
+    expect_equal(
+      calculate_mrt_bin_samplesize_f(tau_t, f_t, g_t, beta, 
+                                     alpha, p_t, gamma, b, FALSE),
+      275)
+  }
+)
+
+
+test_that(
+  "check quadratic example",
+  {
+    expect_equal(
+      calculate_mrt_bin_samplesize_f(tau_t, f_new, g_new, beta_new, 
+                                     alpha_new, p_t, gamma, b, TRUE),
+      32.003286527546599)
+  }
+)
+
+test_that(
+  "check example with different dimension f, g",
+  {
+    expect_equal(
+      calculate_mrt_bin_samplesize_f(tau_t, f_t, g_new, beta, 
+                                     alpha_new, p_t, gamma, b, TRUE),
+      184.43823325060882)
+  }
+)
+
+test_that(
+  "check example with invalid dimension f, g",
+  {
+    expect_warning(
+      calculate_mrt_bin_samplesize_f(tau_t, f_new, g_t, beta_new, 
+                                     alpha, p_t, gamma, b, TRUE),
+      "f should lie in span of g")
+  }
+)
+
+calculate_mrt_bin_samplesize_f(tau_t, f_new, g_t, beta_new, 
+                              alpha, p_t, gamma, b, TRUE)
+
+# compute_m_sigma tests ---------------------------------------------------
+test_that(
+  "check if first error check of invalid probabilities catches error",
+  {
+    expect_error(
+      compute_m_sigma(tau_t, f_t, -g_t, beta, 
+                                     alpha, p_t),
+      message="g_t and alpha values led to invalid probabilities")
+  }
+)
+
+test_that(
+  "check if second error check of invalid probabilities catches error",
+  {
+    expect_error(
+      compute_m_sigma(tau_t, f_t, g_t, beta+1, 
+                                     alpha, p_t, gamma, b),
+      message="f_t and beta values led to invalid probabilities")
+  }
+)
+
+
+# calculate_mrt_bin_power_f tests -----------------------------------------
+
+size1 <- calculate_mrt_bin_samplesize_f(tau_t, f_t, g_t, beta, 
+                          alpha, p_t, gamma, .3, exact=TRUE)
+
+size2 <- calculate_mrt_bin_samplesize_f(tau_t, f_t, g_t, beta, 
+                                        alpha, p_t, gamma, .1, exact=TRUE)
+test_that(
+  "check TQ's sample",
+  {
+    expect_equal(
+      calculate_mrt_bin_power_f(tau_t, f_t, g_t, beta, 
+                                     alpha, p_t, gamma, size1),
+      .3)
+  }
+)
+
+test_that(
+  "check TQ's sample",
+  {
+    expect_equal(
+      calculate_mrt_bin_power_f(tau_t, f_t, g_t, beta, 
+                                alpha, p_t, gamma, size2),
+      .1)
+  }
+)
+
+test_that(
+  "check different dimension f, g",
+  {
+    expect_equal(
+      calculate_mrt_bin_power_f(tau_t, f_t, g_new, beta, 
+                                alpha_new, p_t, gamma, 184.43823325060882),
+      .2)
+  }
+)
+
