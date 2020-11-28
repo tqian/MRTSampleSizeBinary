40ece3983ee922135bd6999e254209d7bd3481b2
Thabat Dahdoul <thabatdahdoulgmail.com@thabats-mbp-2.lan>
2020-11-28 12:59:04 -0800

    added a summary function-but data need documenation

---
+++ R/power_summary.R
@@ -0,0 +1,60 @@
+#' Title This function returns a table of selected sample sizes based on power values
+#'
+#' @param avail_pattern A vector of length T that is the average availability at
+#'   each time point
+#' @param f_t           Defines marginal excursion effect MEE(t) under
+#'   alternative together with beta
+#' @param g_t           Defines success probability null curve together with
+#'   alpha
+#' @param beta          Defines marginal excursion effect MEE(t) under
+#'   alternative together with g_t
+#' @param alpha         Defines success probability null curve together with f_t
+#' @param p_t           Randomization probability at each time point
+#' @param gamma         Desired Type I error
+#'
+#' @return              table of the input values, and sample sizes with some selected power values
+#' @export
+#' @import knitr,
+#'   dplyr
+#'
+#' @examples   power_summary(tau_t_1, f_t_1, g_t_1, beta_1, alpha_1, p_t_1, gamma_1)
+power_summary <- function(avail_pattern,
+                          f_t,
+                          g_t,
+                          beta,
+                          alpha,
+                          p_t,
+                          gamma
+)
+{
+  
+  min_n <- length(beta) + length(alpha)
+  n_vec <- c(seq(min_n +1, min_n + 1000, by = 1))
+  l <- length(n_vec)
+  power_vec <- c(rep(NA, l))
+  
+  for (n_i in n_vec){
+    indx <- which(n_vec == n_i)
+    power_vec[indx] <- calculate_mrt_bin_power_f(avail_pattern, f_t, g_t, beta, alpha, p_t, gamma,n_i)
+    
+  }
+  #power_vec
+  #n_vec
+  power_n_data <- data.frame(power_vec, n_vec)
+  selected_power <- filter( power_n_data, power_vec == c(seq(0.6, 0.95, by = 0.05)))
+  input <- list("Available Pattern" = avail_pattern,
+                             "f(t)" = f_t, 
+                             "g(t)" = g_t, 
+                             "beta" = beta, 
+                            "alpha" = alpha,
+               "Randomization prob" = p_t,
+             "Desired Type I error" = gamma)
+  # kable(input)
+  # kable(selected_power)
+  power_n_data 
+  
+  
+}
+
+power_summary(tau_t_1, f_t_1, g_t_1, beta_1, alpha_1, p_t_1, gamma_1)
+
