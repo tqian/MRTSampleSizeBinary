58cf6a75deead048cf311af581786800f72f22b6
ewongtoi <ewongtoi@gmail.com>
2020-11-28 20:09:14 -0800

    changed way to generate example data.

--- R/example.R
+++ R/example.R
@@ -1,90 +1,77 @@
-#'
-#' @format A list of generated values to use as an example for MRT sample size and power calculations
-#' \describe{
-#'   \item {tau_t_1} {A vector of length T that is the average availability at
-#'   each time point}
-#'   \item {f_t_1} {Defines marginal excursion effect MEE(t) under
-#'   alternative together with beta
-#'   \item {g_t_1} {Defines success probability null curve together with}
-#'   alpha
-#'   \item{beta_1} {Defines marginal excursion effect MEE(t) under}
-#'   alternative together with g_t
-#'   \item{alpha_1} {Defines success probability null curve together with f_t_1}
-#'   \item{p_t_1} {Randomization probability at each time point}
-#'   \item{gamma_1} {Desired Type I error}
-#'   \item{m_matrix_1} { "Bread" of sandwich estimator for variance}
-#'   \item{sigma_matrix_1} {"Meat" of the sandwich estimator}
-#' }
-#' @format 
-#'   "total_dp",
-#'   "tau_t_1",
-#'   "f_t_1",
-#'   "g_t_1",
-#'   "p_t_1",
-#'   "beta_1", 
-#'   "alpha_1", 
-#'   "b_1", 
-#'   "gamma_1",
-#'   "m_matrix_1",
-#'   "sigma_matrix_1",
-#'   "x"
-#'   
-#'   
-#'   
-#' 
-#' 
-#' 
-#' 
-#'  
-
+#' The number of data points
 #' @format numeric value
 #' \describe{
 #' 10
 #' }
 "total_dp"
 
-
+#' Time avail vec
 #' @format vector of length T
 #' \describe{A vector of length T that is the aver availability at each time point.}
 "tau_t_1"
 
+#' Lin eff
 #' @format a 2 by 10 matrix defining a linear effect
 #' \describe{
 #' vector for the null
 #' }
 "f_t_1"
    
-   
+#' vec alte
 #' @format a 2 by 10 matrix defining a linear effect
 #' \describe{
 #' vector for alternative
 #' }
 "g_t_1"
-   
+
+
+#' probs   
 #' @format a length T vector of probability for each time
 #' \describe{
 #' vectro of probs
 #' }
-
 "p_t_1"
 
+#' null
 #' @format beta
 #' \describe{
 #' adsf}
 "beta_1"
 
+#' alt
 #' @format beta
 #' \describe{
 #' adsf}
 "alpha_1"
 
-
+#' bea
 #' @format beta
 #' \describe{
 #' adsf}
 "gamma_1"
 
+#' a'kdfjs
 #' @format beta
 #' \describe{
 #' adsf}
 "b_1"
+
+#' a'kdfjs
+#' @format beta
+#' \describe{
+#' adsf}
+"x"
+
+#' a'kdfjs
+#' @format beta
+#' \describe{
+#' adsf}
+"sigma_matrix_1"
+
+#' a'kdfjs
+#' @format beta
+#' \describe{
+#' adsf}
+"m_matrix_1"
+
+
