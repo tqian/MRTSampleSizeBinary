4706f1e3d9c1108882de474820ffe7b214bc86da
ewongtoi <ewongtoi@gmail.com>
2020-11-29 17:12:41 -0800

    helper function that is used in check for linearly dependent p_t\times f_t and g_t

---
+++ R/is_full_column_rank.R
@@ -0,0 +1,13 @@
+#' Check if a matrix is full column rank.
+#'
+#' @param mat A matrix.
+#'
+#' @return Boolean TRUE/FALSE for if matrix is full column rank.
+#' @importFrom Matrix rankMatrix
+#' @export
+#' @examples   is_full_column_rank(diag(4))
+is_full_column_rank <- function(mat) {
+  # Full column rank if only if all columns are linearly independent
+  # See: https://en.wikipedia.org/wiki/Rank_(linear_algebra)
+  return(rankMatrix(mat)[[1]] == ncol(mat))
+}
