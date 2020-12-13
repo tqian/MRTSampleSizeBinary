e736b920b8a7f5a513d3b31eb748791513deb65b
Tianchen Qian <qiantianchen.thu@gmail.com>
2020-12-12 20:47:05 -0800

    Make alpha_1 and beta_1 vectors instead of matrices

--- data-raw/input_example.R
+++ data-raw/input_example.R
@@ -4,7 +4,7 @@
 set.seed(1)
 
 total_dp <- 10 # total number of decision points
-# p and q are not directly used in the function, so I commented them out.
+# p and q are not directly used in the function; they are defined when we define alpha_1 and beta_1.
 # p <- 2
 # q <- 2
 
@@ -15,20 +15,20 @@ gamma_1 <- 0.05 # type I error
 b_1 <- 0.2 # type II error; power = 1 - b
 
 ### specify g_t and alpha ###
-g_t_1 <- cbind(rep(1, total_dp), 1:total_dp)  # f_t = (1,g_t)
-alpha_1 <- as.matrix(c(-0.2, -0.1), ncol = 1)
+g_t_1 <- cbind(rep(1, total_dp), 1:total_dp)  # g_t = (1, t)
+alpha_1 <- c(-0.2, -0.1)
 
 # checking that probability E(Y_{t+1} = 1 | I_t = 1, A_t = 0) should always be between 0 and 1.
-mu0_t_1 <- exp(g_t_1 %*% alpha_1) # E(Y_{t+1} = 1 | I_t = 1, A_t = 0) for t = 1,...,total_dp
+mu0_t_1 <- exp(g_t_1 %*% as.matrix(alpha_1, ncol = 1)) # E(Y_{t+1} = 1 | I_t = 1, A_t = 0) for t = 1,...,total_dp
 mu0_t_1 # look at the mu0_t values
 stopifnot(all(mu0_t_1 < 1) & all(mu0_t_1 > 0))
 
 ### specify f_t and beta ###
 f_t_1 <- cbind(rep(1, total_dp), 1:total_dp)  # f_t = (1, t)
-beta_1 <- as.matrix(c(0.15, - 0.01), ncol = 1)
+beta_1 <- c(0.15, - 0.01)
 
 # checking that probability E(Y_{t+1} = 1 | I_t = 1, A_t = 1) should always be between 0 and 1.
-mee_t <- f_t_1 %*% beta_1 # MEE(t) for t = 1,...,total_dp
+mee_t <- f_t_1 %*% as.matrix(beta_1, ncol = 1) # MEE(t) for t = 1,...,total_dp
 mu1_t_1 <- mu0_t_1 * exp(mee_t) # E(Y_{t+1} = 1 | I_t = 1, A_t = 1) for t = 1,...,total_dp
 mu1_t_1 # look at the mu1_t values
 stopifnot(all(mu1_t_1 < 1) & all(mu1_t_1 > 0))
